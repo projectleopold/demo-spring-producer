0a0f158a016619809c0854037b605f579bbac239
Grig Alex <alexengrigdev@gmail.com>
2023-02-24 11:35:03 +0300

    Init project

---
+++ src/test/java/io/github/projectleopold/demo/springproducer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.projectleopold.demo.springproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
